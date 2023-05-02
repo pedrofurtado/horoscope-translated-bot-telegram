8f1344f55c4d4af0adc19e549bff5c2213986fa0
Pedro Furtado <pedro.furtado@locaweb.com.br>
2023-05-02 18:41:46 -0300

    fix code

--- api/index.py
+++ api/index.py
@@ -15,51 +15,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Horoscope Translated Bot Telegram'
-
-@app.route('/about')
-def about():
     return 'Horoscope Translated Bot Telegram - See more details in https://github.com/pedrofurtado/horoscope-translated-bot-telegram'
 
-@app.route('/set_webhook', methods=['GET', 'POST'])
+@app.route('/setup_webhook', methods=['GET', 'POST'])
 def set_webhook():
     s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
+
     if s:
-        return "webhook setup ok"
+        return "Webhook setup ok"
     else:
-        return "webhook setup failed"
+        return "Webhook setup failed"
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    telegram_message = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
+    chat_id = telegram_message.message.chat.id
+    message_id = telegram_message.message.message_id
 
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    # for debugging purposes only
-    print("got text message :", text)
-    # the first time you chat with the bot AKA the welcoming message
+    text = telegram_message.message.text.encode('utf-8').decode()
+
+    print('Telegram message received: Text [{message_text}] | Chat ID {chat_id} | Message ID {message_id} | Telegram object {telegram_object}'.format(message_text=text, chat_id=chat_id, message_id=message_id, telegram_object=repr(telegram_message))
+
     if text == "/start":
-        # print the welcoming message
         bot_welcome = """
-        Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+        Welcome to HoroscopeTranslatedBot!
         """
-        # send the welcoming message
-        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=message_id)
     else:
         try:
-            # clear the message we got from any non alphabets
-            text = re.sub(r"\W", "_", text)
-            # create the api link for the avatar based on http://avatars.adorable.io/
-            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-            # reply with a photo to the name the user sent,
-            # note that you can send photos by url and telegram will fetch it for you
-            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+            photo_url = "https://picsum.photos/200/300"
+            bot.sendPhoto(chat_id=chat_id, photo=photo_url, reply_to_message_id=message_id)
         except Exception:
-            # if things went wrong
-            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+            bot.sendMessage(chat_id=chat_id, text="There was a problem!", reply_to_message_id=message_id)
 
     return 'ok'
