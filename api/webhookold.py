ab7220b6ae423c448846caf00f6db7b39204a8c9
Pedro Furtado <pedro.furtado@locaweb.com.br>
2023-05-02 22:24:02 -0300

    fix

--- api/webhookold.py
+++ api/webhookold.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 import telegram
 import os
 
-#import telegram.ext
 import requests
 
 def telegram_bot_url():
@@ -114,10 +113,43 @@ def setup_webhook():
         return "Webhook setup failed"
 
 @app.route('/{}'.format(telegram_bot_token()), methods=['POST'])
-def webhook():
-    # https://github.com/python-telegram-bot/v13.x-wiki/wiki/Webhooks
-    # https://github.com/aarzaary/telegram-bot-vercel-python/blob/48f1905bbfc0af25e11c376b817e9e91ba7e0174/example/bot.py#L15
-    updater = create_updater()
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-    updater.dispatcher.process_update(update)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    # for debugging purposes only
+    print("got text message :", text)
+    # the first time you chat with the bot AKA the welcoming message
+    if text == "/start":
+        # print the welcoming message
+        bot_welcome = """
+        Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+        """
+        # send the welcoming message
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+    else:
+        try:
+            # clear the message we got from any non alphabets
+            text = re.sub(r"\W", "_", text)
+            # create the api link for the avatar based on http://avatars.adorable.io/
+            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+            # reply with a photo to the name the user sent,
+            # note that you can send photos by url and telegram will fetch it for you
+            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+        except Exception:
+            # if things went wrong
+            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+
     return 'ok'
+# def webhook():
+#     # https://github.com/python-telegram-bot/v13.x-wiki/wiki/Webhooks
+#     # https://github.com/aarzaary/telegram-bot-vercel-python/blob/48f1905bbfc0af25e11c376b817e9e91ba7e0174/example/bot.py#L15
+#     updater = create_updater()
+#     update = telegram.Update.de_json(request.get_json(force=True), bot)
+#     updater.dispatcher.process_update(update)
+#     return 'ok'
