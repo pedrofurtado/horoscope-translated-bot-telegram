557c71573974bad4db29645118424ca154014262
Pedro Furtado <pedro.furtado@locaweb.com.br>
2023-05-02 22:34:50 -0300

    asd

--- api/webhookold.py
+++ api/webhookold.py
@@ -122,9 +122,9 @@ def respond():
     text = update.message.text.encode('utf-8').decode()
     print("got text message :", text)
 
-    updater = telegram.ext.Updater(telegram_bot_token(), use_context=True)
-    disp = updater.dispatcher
-    create_handlers(disp)
+    # updater = telegram.ext.Updater(telegram_bot_token(), use_context=True)
+    # disp = updater.dispatcher
+    # create_handlers(disp)
 
     if text == "/start":
         # print the welcoming message
