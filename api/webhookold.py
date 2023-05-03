9076861c8fa7132617e90f1868aebf2371b0b0f4
Pedro Furtado <pedro.furtado@locaweb.com.br>
2023-05-02 21:54:01 -0300

    fixes

--- api/webhookold.py
+++ api/webhookold.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import telegram
 import os
-from utils import funcs
+from myutils import funcs
 
 global bot
 bot = telegram.Bot(token=funcs.telegram_bot_token())
@@ -25,11 +25,11 @@ def setup_webhook():
     else:
         return "Webhook setup failed"
 
-# @app.route('/{}'.format(funcs.telegram_bot_token()), methods=['POST'])
-# def webhook():
-#     # https://github.com/python-telegram-bot/v13.x-wiki/wiki/Webhooks
-#     # https://github.com/aarzaary/telegram-bot-vercel-python/blob/48f1905bbfc0af25e11c376b817e9e91ba7e0174/example/bot.py#L15
-#     updater = funcs.create_updater()
-#     update = telegram.Update.de_json(request.get_json(force=True), bot)
-#     updater.dispatcher.process_update(update)
-#     return 'ok'
+@app.route('/{}'.format(funcs.telegram_bot_token()), methods=['POST'])
+def webhook():
+    # https://github.com/python-telegram-bot/v13.x-wiki/wiki/Webhooks
+    # https://github.com/aarzaary/telegram-bot-vercel-python/blob/48f1905bbfc0af25e11c376b817e9e91ba7e0174/example/bot.py#L15
+    updater = funcs.create_updater()
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    updater.dispatcher.process_update(update)
+    return 'ok'
